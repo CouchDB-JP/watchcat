89e67e80c675cd74081025d765af6e899d441974
Kouhei Maeda <mkouhei@palmtb.net>
2011-04-27 23:06:57 +0900

    fix date format.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- deletephoto.sh
+++ deletephoto.sh
@@ -2,4 +2,4 @@
 
 USER=GoogleAccount
 
-google -u $USER picasa delete --query "`date --date '1 day ago' +\"%Y%m%d\"`" --yes
+google -u $USER picasa delete --query="`date --date '8 hour ago' +\"%Y%m%d%H\"`" --yes
