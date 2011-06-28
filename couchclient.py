024841b4d1b04e18a5cac898b32996187f35c459
Kouhei Maeda <mkouhei@palmtb.net>
2011-06-28 18:15:21 +0900

    getting date infomation from filename.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- couchclient.py
+++ couchclient.py
@@ -4,10 +4,10 @@ class Photos:
     '''Listing photo images taken with motion.'''
     def listFiles(self):
         import glob, os.path
-        for f in glob.glob("*-??????????????-??-?.jpg"):
-            if os.path.isfile(f):
-                print f
-                self.encodeBase64(f)
+        for self.filename in glob.glob("*-??????????????-??-?.jpg"):
+            if os.path.isfile(self.filename):
+                self.encodeBase64(self.filename)
+                self.getDate()
                 self.encodeJson()
                 print self.doc
                 
@@ -17,9 +17,31 @@ class Photos:
         import base64
         self.image_base64 = base64.encodestring(open(filepath,"rb").read())
 
+    '''Getting date info from filename.'''
+    def getDate(self):
+        import re
+        j = re.match('^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})',
+                     re.match('(\d+)-(\d+)-(\d+)-(\d).jpg', self.filename)
+                     .group(2))
+        self.year = j.group(1)
+        self.month = j.group(2)
+        self.date = j.group(3)
+        self.hour = j.group(4)
+        self.minute = j.group(5)
+        self.second = j.group(6)
+
     '''Serialize JSON Document.'''
     def encodeJson(self):
-        self.doc = {"image":self.image_base64}
+        self.doc = {
+            "filename":self.filename,
+            "image":self.image_base64,
+            "year":self.year,
+            "month":self.month,
+            "date":self.date,
+            "hour":self.hour,
+            "minute":self.minute,
+            "second":self.second
+            }
 
 
 
