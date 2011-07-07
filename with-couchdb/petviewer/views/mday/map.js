f877f43e13838ba2aa1c39cf93c439acd777f751
Kouhei Maeda <mkouhei@palmtb.net>
2011-07-07 14:24:15 +0900

    find today photo, sort key by hour.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- with-couchdb/petviewer/views/mday/map.js
+++ with-couchdb/petviewer/views/mday/map.js
@@ -1,5 +1,17 @@
 function(doc) {
-    if(doc) {
-	emit(doc.mday,doc);
-    }
+
+    // get current time.
+    function getDate() {
+	var dtNow = new Date();
+	this.dtYear = dtNow.getFullYear();
+	this.dtMon = dtNow.getMonth() + 1;
+	this.dtMday = dtNow.getDate();
+    };
+
+    var date = new getDate();
+    if(doc.thumbnail) {
+	if ((doc.year == date.dtYear) && (doc.mon == date.dtMon) 
+	    && (doc.mday == date.dtMday))
+	    emit(doc.hour, doc);
+    };
 }
